392c2edcfc33db1836ce9cf3377cd3b76a05664b
passer <ipasser@sina.cn>
2019-05-29 19:25:06 +0800

    modify

---
+++ target/test-classes/help/musicapi_discuss_comment.sql
@@ -0,0 +1,2 @@
+INSERT INTO musicapi.discuss_comment (user_id, content, create_time, event_id) VALUES ('12121112', 'I am a test', '2019-05-28 22:00:58', 3);
+INSERT INTO musicapi.discuss_comment (user_id, content, create_time, event_id) VALUES ('12121112', 'I am also a test', '2019-05-28 22:01:29', 3);
